df3e5b3915f182b3a834cda05975c8f7eccc7aee
tombro27 <singhtomar.prakhar@gmail.com>
2021-05-28 16:34:04 +0530

    Added login and signup pages

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] ='14401501c7c142d2b921237778274fb5'
 
 posts = [
     {
@@ -27,6 +30,24 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Registration Successful. Account created for { form.username.data }!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else :
+            flash('Login failed. Please Check email and password', 'danger')        
+    return render_template('login.html',title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
