f44b8a014a3ac806252023ef022b0282b8ec915b
tombro27 <singhtomar.prakhar@gmail.com>
2021-05-31 12:56:40 +0530

    Added User Authentication-register and login

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, url_for, flash, redirect
+from flask_blog import app, db, bcrypt
 from flask_blog.forms import RegistrationForm, LoginForm
 from flask_blog.models import User, Post
-from flask_blog import app
+from flask_login import login_user
+
 
 posts = [
     {
@@ -33,16 +35,21 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Registration Successful. Account created for { form.username.data }!','success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account Created! You can now login','success')
+        return redirect(url_for('login'))
     return render_template('register.html',title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else :
             flash('Login failed. Please Check email and password', 'danger')        
