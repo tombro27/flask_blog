f7ef6af8ba15f2b762d81ad68778c1869a21b9c3
tombro27 <singhtomar.prakhar@gmail.com>
2021-06-02 13:46:10 +0530

    Added option to Update Username and Email

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, url_for, flash, redirect
 from flask_blog import app, db, bcrypt
-from flask_blog.forms import RegistrationForm, LoginForm
+from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -61,7 +61,18 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('Account.html',title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file) 
+    return render_template('Account.html',title='Account', image_file=image_file, form=form)
